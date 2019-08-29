f6a409f8b27186a10962bee2cab09c0d59d51229
campovski <campovski@gmail.com>
2019-08-29 21:18:04 +0200

    Add basic server.js and frontend files

---
+++ app/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// Configure nunjucks to use express app
+nunjucks.configure('views', {
+    express: app,
+    autoescape: true
+});
+app.set('view engine', 'html');
+
+// Routes
+app.get('/', (req, res) => res.render('index.html'));
+app.get('/game', (req, res) => res.render('game/game.html', { gameId: '22213' }));
+
+// Start the server
+app.listen(port, () => { console.log(`Ex Cerebrum server listening on port ${port}!`); });
